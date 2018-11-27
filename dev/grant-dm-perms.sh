f77a94cc7e191f19c148797b2d3c87469e9e0786
Ximin Luo <infinity0@debian.org>
2018-11-27 01:30:48 -0800

    dev/grant-dm-perms: take into account uploaders from debcargo.toml too

--- dev/grant-dm-perms.sh
+++ dev/grant-dm-perms.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 # Grant DM upload permissions on all crates maintained by a DM.
-#
-# TODO: use uploaders when that feature gets into debcargo
-# https://salsa.debian.org/rust-team/debcargo/issues/10
 
 m="$1"
 shift
-grep -l "$m" src/*/debian/copyright \
-| sed -nre 's,src/(.*)/debian/copyright,\1,gp' \
+grep -l "$m" src/*/debian/copyright src/*/debian/debcargo.toml \
+| sed -nre 's,src/(.*)/debian/.*,\1,gp' \
+| sort -u \
 | dev/filter-in-debian.sh \
 | sed -nre 's/^(.*)\s1$/rust-\1/gp' \
 | xargs -r dcut "$@" dm --uid "$m" --allow
