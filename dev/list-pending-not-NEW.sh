444a88a59e2e347cc0f4e578cc1a50b10f6cc81b
Ximin Luo <infinity0@debian.org>
2019-07-20 15:15:40 -0700

    dev: fix list-pending-not-NEW.sh to account for package names with explicit versions

--- dev/list-pending-not-NEW.sh
+++ dev/list-pending-not-NEW.sh
@@ -7,7 +7,7 @@ git fetch origin --prune
 comm -13 \
  <(curl -s https://ftp-master.debian.org/new.html | \
    grep '<td class="package">rust-' | \
-   sed -nre 's/.*\brust-([-A-Za-z0-9]+)\b.*/\1/gp' | \
+   sed -nre 's/.*\brust-([-A-Za-z0-9.]+)\b.*/\1/gp' | \
    sort) \
  <(git branch --list -r 'origin/pending-*' --format='%(refname)' | \
    sed -e 's,refs/remotes/origin/pending-,,g' | \
