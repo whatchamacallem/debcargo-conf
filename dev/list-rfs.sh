4b6bb6a3533f394a1963b1b7698633bf623f5674
Sylvestre Ledru <sylvestre@debian.org>
2019-08-15 10:51:05 +0200

    If RFS has some content, show it + skip the pending branches

--- dev/list-rfs.sh
+++ dev/list-rfs.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 for i in src/*/debian/RFS; do
-	basename $(dirname $(dirname $i))
+	pkg=$(basename $(dirname $(dirname $i)))
+    if test -n "$(git --no-pager branch --remotes -l origin/pending-$pkg)"; then
+        # If a pending branch exists, skip it
+        continue
+    fi
+    echo $pkg
+    # trim the content
+    content=$(cat $i|xargs echo -n)
+    if test -n "$content"; then
+        echo "=> $content"
+    fi
 done
