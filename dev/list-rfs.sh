75ed87db2a349898ed71316d809a780fde80d911
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2019-08-16 06:57:44 +0200

    Fix shellcheck remarks in dev/list-rfs.sh

--- dev/list-rfs.sh
+++ dev/list-rfs.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 for i in src/*/debian/RFS; do
-	pkg=$(basename $(dirname $(dirname $i)))
-    if test -n "$(git --no-pager branch --remotes -l origin/pending-$pkg)"; then
+    pkg=$(basename "$(dirname "$(dirname "$i")")")
+    if test -n "$(git --no-pager branch --remotes -l origin/pending-"$pkg")"; then
         # If a pending branch exists, skip it
         continue
     fi
-    echo $pkg
+    echo "$pkg"
     # trim the content
-    content=$(cat $i|xargs echo -n)
+    content=$(xargs echo -n < "$i")
     if test -n "$content"; then
         echo "=> $content"
     fi
