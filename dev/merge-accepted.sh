705e6d0b1eb8150413e875288be35e07946705af
Ximin Luo <infinity0@debian.org>
2019-11-28 00:10:35 +0000

    Add a merge-accepted script

---
+++ dev/merge-accepted.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+packages=( $(dev/list-pending-not-NEW.sh | dev/filter-package-in-debian.sh | grep -v ' 0$' | cut '-d ' -f1) )
+set -x
+
+git merge "${packages[@]/#/pending-}"
+git push origin master "${packages[@]/#/:pending-}"
+dev/sync-pending-branches.sh
