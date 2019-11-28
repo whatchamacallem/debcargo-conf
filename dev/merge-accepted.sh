90f3bab9fc5d90e765e401c6e7ee4ef18949f2d5
Ximin Luo <infinity0@debian.org>
2019-11-28 00:28:47 +0000

    More script tweaks regarding syncing with master

--- dev/merge-accepted.sh
+++ dev/merge-accepted.sh
@@ -2,7 +2,13 @@
 set -e
 
 packages=( $(dev/list-pending-not-NEW.sh | dev/filter-package-in-debian.sh | grep -v ' 0$' | cut '-d ' -f1) )
+
+if [ -z "$packages" ]; then exit 0; fi
 set -x
+
+git fetch origin --prune
+git diff --quiet origin/master || \
+{ echo >&2 "Please sync with origin/master before running this."; exit 1; }
 
 git merge "${packages[@]/#/pending-}"
 git push origin master "${packages[@]/#/:pending-}"
