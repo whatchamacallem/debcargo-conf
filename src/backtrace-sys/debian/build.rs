015a91d8ab3f55eeae5940d172ba48aa7a8e4f98
Ximin Luo <infinity0@debian.org>
2019-12-05 22:27:40 +0000

    backtrace-sys: auto-detect Debian cross-compiling

--- src/backtrace-sys/debian/build.rs
+++ src/backtrace-sys/debian/build.rs
@@ -1,10 +1,16 @@
 use std::process::Command;
+use std::env;
 
 fn main() {
+    println!("cargo:rerun-if-env-changed={}", "DEB_HOST_GNU_TYPE");
+    let exe = env::var("DEB_HOST_GNU_TYPE")
+        .map(|t| t.to_string() + "-gcc")
+        .unwrap_or_else(|_| String::from("gcc"));
+    let cmd = format!("{} -print-search-dirs | sed -ne 's/^install: //p'", exe).to_string();
     let search_dir = Command::new("sh")
-        .args(&["-c", "gcc -print-search-dirs | sed -ne 's/^install: //p'"])
+        .args(&["-c", &cmd])
         .output().expect("failed to find gcc install dir").stdout;
     println!("cargo:rustc-link-lib=static=backtrace");
-    println!("cargo:rustc-link-search=native={}", String::from_utf8(search_dir).unwrap().trim_right());
+    println!("cargo:rustc-link-search=native={}", String::from_utf8(search_dir).unwrap().trim_end());
     println!("dh-cargo:deb-built-using=backtrace=0~={}", "libgcc-[0-9]+-dev .*");
 }
