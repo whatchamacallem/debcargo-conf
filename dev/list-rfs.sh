0558696d52a01079128a669432130c68e9b043c5
Ximin Luo <infinity0@debian.org>
2019-12-23 02:10:07 +0000

    dev/list-rfs.sh: add dates

--- dev/list-rfs.sh
+++ dev/list-rfs.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 for i in src/*/debian/RFS; do
+    echo "$(git log -1 --pretty="format:%ct" "$i")" "$i"
+done | sort  | while read t i; do
     pkg=$(basename "$(dirname "$(dirname "$i")")")
     if test -n "$(git --no-pager branch --remotes -l origin/pending-"$pkg")"; then
         # If a pending branch exists, skip it
         continue
     fi
-    echo "$pkg"
+    echo "$(date -d@"$t") $pkg"
     # trim the content
     content=$(xargs echo -n < "$i")
     if test -n "$content"; then
