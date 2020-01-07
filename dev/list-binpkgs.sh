c8c9b586198f9038b5774a8beb2d7f24eab34bcd
Ximin Luo <infinity0@debian.org>
2020-01-07 21:45:27 +0000

    add script for listing packages with rust binaries

---
+++ dev/list-binpkgs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# List all packages that produce rust binaries, from this repo.
+grep -l "\[packages.bin\]" src/*/debian/debcargo.toml \
+  | cut -d/ -f2 \
+  | sed -e 's/^/rust-/g' \
+  | tr '\n' ',' \
+  | sed -e 's/,$/\n/'
