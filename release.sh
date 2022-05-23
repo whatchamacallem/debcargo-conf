ec606323a22e4aad5ba37541fcf5564b003069fe
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2022-05-23 20:14:26 +0200

    Fix concatenation of commands proposed by release.sh
    In a short correspondence in the #debian-rust chatroom, dkg and I
    coincided that the concatenation of the `git merge -` command should be
    a `&&` instead of the `&`, which would make it happen unconditionally
    and possibly too early, and we don't think this is the intention here.

--- release.sh
+++ release.sh
@@ -203,11 +203,11 @@ cat <<eof
 Since the source package is already in Debian and this version does not introduce
 new binaries, then you can just go ahead and directly dput the source package.
 
-  cd build && dput ${DEBSRC}_${DEBVER}_source.changes && git checkout - & git merge -
+  cd build && dput ${DEBSRC}_${DEBVER}_source.changes && git checkout - && git merge -
 
 If you want to build and test it, run:
 
-  cd build && ./build.sh $CRATE && dput ${DEBSRC}_${DEBVER}_source.changes && git checkout - & git merge -
+  cd build && ./build.sh $CRATE && dput ${DEBSRC}_${DEBVER}_source.changes && git checkout - && git merge -
 
 For your reference, this source package builds $(echo "$upload_bin_packages" | wc -l) binary package(s):
 $upload_bin_packages
