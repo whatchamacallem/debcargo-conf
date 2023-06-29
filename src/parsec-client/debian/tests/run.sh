8c0c8ebb9814ceba0256afa4c6b67fbd0f13fc69
Emanuele Rocca <ema@debian.org>
2023-06-29 10:59:30 +0200

    parsec-client: add parsec-service to test_depends

--- src/parsec-client/debian/tests/run.sh
+++ src/parsec-client/debian/tests/run.sh
@@ -16,6 +16,4 @@ echo $! > /run/parsec/parsec.pid
 
 sleep 3
 
-version="$(apt-cache policy librust-parsec-client-dev | grep -F 'Installed: ' | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')"
-
 /usr/share/cargo/bin/cargo-auto-test $@
