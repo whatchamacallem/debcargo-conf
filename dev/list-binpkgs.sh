d3a59ccee8348b82e1443871689a6a7559662714
Sylvestre Ledru <sylvestre@debian.org>
2024-05-16 19:32:27 +0200

    dev/list-binpkgs.sh: If the first command line argument is set to --uploaded, only show programs that have been uploaded

--- dev/list-binpkgs.sh
+++ dev/list-binpkgs.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 # List all packages that produce rust binaries, from this repo.
+
+# If the first command line argument is set to --uploaded, only show programs that have
+# been uploaded
+check_uploaded=${1:-disable}
+
 grep -l "\[packages.bin\]" src/*/debian/debcargo.toml \
   | cut -d/ -f2 \
-  | sed -e 's/^/rust-/g' \
+  | while read pkg; do
+      # If check is enabled and 'uploaded' is found, or if check is disabled
+      if [ "$check_uploaded" == "--uploaded" ] && grep -q "unstable" "src/$pkg/debian/changelog"; then
+          echo "rust-$pkg"
+      elif [ "$check_uploaded" == "disable" ]; then
+          echo "rust-$pkg"
+      fi
+    done \
   | tr '\n' ',' \
   | sed -e 's/,$/\n/'
