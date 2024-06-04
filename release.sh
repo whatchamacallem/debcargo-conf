b4ca9a0cc2ab8a421eef6665d8cbc65658021c70
Blair Noctis <n@sail.ng>
2024-06-04 10:06:37 +0000

    release.sh: include version in commit message

--- release.sh
+++ release.sh
@@ -143,7 +143,7 @@ if ! ( cd build && SOURCEONLY=1 ./build.sh "$CRATE" $VER ); then
 		"- packaged version is out-of-date => run \`./update.sh $*\`"
 fi
 
-git commit -m "Release package $PKGNAME"
+git commit -m "$PKGNAME: release $REALVER"
 
 DEBVER=$(dpkg-parsechangelog -l $BUILDDIR/debian/changelog -SVersion)
 DEBSRC=$(dpkg-parsechangelog -l $BUILDDIR/debian/changelog -SSource)
