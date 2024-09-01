f4780128ba9a9f9a4f5816e003e71003954b1285
Julian Gilbey <jdg@debian.org>
2024-09-01 19:40:46 +0100

    Don't advise merging pending-release branch into master before it is ACCEPTED

--- release.sh
+++ release.sh
@@ -208,11 +208,11 @@ cat <<eof
 Since the source package is already in Debian and this version does not introduce
 new binaries, then you can just go ahead and directly dput the source package.
 
-  cd build && dput ${DEBSRC}_${DEBVER}_source.changes && git checkout - && git merge - && cd -
+  cd build && dput ${DEBSRC}_${DEBVER}_source.changes && cd - && git checkout master
 
 If you want to build and test it, run:
 
-  cd build && ./build.sh $CRATE && dput ${DEBSRC}_${DEBVER}_source.changes && git checkout - && git merge - && cd -
+  cd build && ./build.sh $CRATE && dput ${DEBSRC}_${DEBVER}_source.changes && cd - && git checkout
 
 For your reference, this source package builds $(echo "$upload_bin_packages" | wc -l) binary package(s):
 $upload_bin_packages
