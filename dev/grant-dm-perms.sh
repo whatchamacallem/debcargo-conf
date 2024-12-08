7089ba58db6784f4a1bd11aba2db173e6a1253dc
Holger Levsen <holger@layer-acht.org>
2024-12-08 19:58:07 +0100

    grant-dm-perms.sh: apply some more duct tape to make it more robust
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- dev/grant-dm-perms.sh
+++ dev/grant-dm-perms.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Grant DM upload permissions on all crates maintained by a DM.
 
+# this script needs dcut-ng
+# and deb-src entries in /etc/apt/sources.list
+
 if [ "$#" -ne 1 ]; then
     echo "Syntax: $0 <email>"
     exit 1
@@ -8,9 +11,15 @@ fi
 
 m="$1"
 shift
-grep -l "$m" src/*/debian/copyright src/*/debian/debcargo.toml \
+pkgs=$(grep -l "$m" src/*/debian/copyright src/*/debian/debcargo.toml \
 | sed -nre 's,src/(.*)/debian/.*,\1,gp' \
 | sort -u \
 | dev/filter-package-in-debian.sh \
-| sed -nre 's/^(.*)\s[1-9][0-9]*$/rust-\1/gp' \
+| sed -nre 's/^(.*)\s[1-9][0-9]*$/rust-\1/gp')
+
+echo "Do you really want to grant DM upload permissions to $m for these packages:"
+echo $pkgs
+echo
+echo "Press enter to grant DM upload permissions to $m for these packages, else press CTRL-C to abort."
+read a
 | xargs -t -r dcut "$@" dm --uid "$m" --allow
